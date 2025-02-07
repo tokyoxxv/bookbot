2e5c8bc1bddd273286920520ccfb32d350c5ec65
tokyoxxv <felix_25@inbox.ru>
2025-02-07 19:26:41 +0500

    final version

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-def read_file(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
-
-def word_counts(text):
-    count = text.split()
-    return len(count)
-
 def main():
-    path_to_file = "books/frankenstein.txt"  # Path to your file
-    content = read_file(path_to_file)
-    print(content)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} found in the document\n")
+    for key, value in chars_dict.items():
+        if key.isalpha():
+            print(f"The '{key}' character was found {value} times")
+    print("--- End report ---")
 
-    words = word_counts(content)
-    print(f"The Frankenstein book contains {words} words.")
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
